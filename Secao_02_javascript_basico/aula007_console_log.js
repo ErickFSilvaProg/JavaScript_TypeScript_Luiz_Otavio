af983c3ebcaa262e3faee36171fd54f6c4029a9a
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-04 20:34:10 -0300

    JavaScript_TypeScript_Luiz_Otavio 04/07/2026 (noite)

---
+++ Secao_02_javascript_basico/aula007_console_log.js
@@ -0,0 +1,16 @@
+/*
+    — console.log()
+    
+        ↪ Método "log" que está dentro objeto "console".
+    
+    No javascrit o ponto-e-vírgula é opcional.
+    A função console.log() dá uma quebra de linha altomaticamente.
+*/
+
+
+// Valores literais.
+
+// Strings:
+console.log('Erick Ferreira');
+console.log("Programador Web");
+console.log(`JavaScript/Typescript`);
