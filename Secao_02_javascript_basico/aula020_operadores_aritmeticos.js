c9da0006a719f529d73b9de1e8f8ee09ea779e30
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-09 16:55:55 -0300

    JavaScript_TypeScript_Luiz_Otavio 09/07/2026 (tarde)

---
+++ Secao_02_javascript_basico/aula020_operadores_aritmeticos.js
@@ -0,0 +1,34 @@
+/*
+    Operadores aritméticos:
+
+        () → Parênteses
+        ++ → Incremento 
+        -- → Decremento
+        ** → Potenciação
+        *  → Multiplicação
+        /  → Divisão
+        %  → Módulo (resto da divisão)
+        +  → Adição ou concatenação
+        -  → Subtração
+        <, <=, >, >=  → Comparadores Relacionais
+        ==, !=, ===, !==  → Igualdade e Desigualdade
+        (E / OU): &&, ||  → Lógicos
+        =, +=, -=, *=, /=  Atribuição
+
+    Obs.: Quando operadores têm a mesma precedência (ex: multiplicação e divisão), a regra de associatividade entra em ação, sendo geralmente avaliados da esquerda para a direita
+*/
+
+
+// Variáveis:
+const number1 = 5;
+const number2 = 10;
+const name = 'Carlos';
+
+
+// Adição:
+console.log('- Adição:')
+console.log(number1 + number2);
+
+
+// Concatenação
+console.log();
