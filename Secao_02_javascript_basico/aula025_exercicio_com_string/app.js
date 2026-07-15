79ecbdf15f685b7aabf00ffa7eda5860ea6d29e2
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-14 22:24:16 -0300

    JavaScript_TypeScript_Luiz_Otavio 14/07/2026 (noite)

--- Secao_02_javascript_basico/aula025_exercicio_com_string/app.js
+++ Secao_02_javascript_basico/aula025_exercicio_com_string/app.js
@@ -1,4 +1,14 @@
+// Entrada de dados:
 const nome = prompt('Digite seu nome completo:');
+// const nome = 'Erick Ferreira da Silva';
 
-document.body.innerHTML += `O seu nome é: ${nome}<br>`;
-document.body.innerHTML += ``;
+// Processamento e saída:
+document.body.innerHTML += `O seu nome completo é: <b>${nome}</b><br>`;
+document.body.innerHTML += `Seu nome tem: <b>${nome.length}</b> letras<br>`;
+document.body.innerHTML += `A segunda letra do seu nome é: <b>${nome.charAt(1)}</b><br>`;
+document.body.innerHTML += `Qual o primeiro índice da letra "a" do seu nome? <b>${nome.indexOf('a')}</b><br>`;
+document.body.innerHTML += `Qual o último índice da letra "a" no seu nome? <b>${nome.lastIndexOf('a')}</b><br>`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: <b>${nome.slice(-3)}</b><br>`;
+document.body.innerHTML += `As palavras do seu nome são: <b>${nome.split(' ')}</b><br>`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: <b>${nome.toUpperCase()}</b><br>`;
+document.body.innerHTML += `Seu nome com letras minúsculas: <b>${nome.toLowerCase()}</b><br>`;
