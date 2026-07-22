89cdd2ba483fde9e203f320461066863632a5ab1
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-22 19:14:25 -0300

    JavaScript_TypeScript_Luiz_Otavio 22/07/2026 (noite)

--- Secao_02_javascript_basico/aula025_mais_sobre_numbers.js
+++ Secao_02_javascript_basico/aula025_mais_sobre_numbers.js
@@ -9,6 +9,9 @@
 let num1 = 1;
 let num2 = 2.5;
 let num3 = 1024;
+let num4 = 10.5789551255547;
+let num5 = '2026';
+let texto = 'Lorem ipsum';
 
 
 // Convertendo número em string:
@@ -20,4 +23,25 @@ console.log();
 // Representação binária de um número:
 console.log('// Representação binária de um número:');
 console.log(num3.toString(2));
+console.log();
+
+
+// Limitar as casas decimais:
+console.log('// Limitar as casas decimais:');
+console.log(num4.toFixed(2));
+console.log();
+
+
+// Checando se o valor é um inteiro:
+console.log('// Checando se o valor é um inteiro:');
+console.log(Number.isInteger(num1)); // Número inteiro.
+console.log(Number.isInteger(num2)); // Número com ponto flutuante.
+console.log(Number.isInteger(num5)); // Número em formato string.
+console.log();
+
+
+// Checando se o valor é um NaN (Not a Number):
+console.log('// Checando se o valor é um NaN (Not a Number):');
+console.log(Number.isNaN(num1));
+console.log(Number.isNaN(texto));
 console.log();
