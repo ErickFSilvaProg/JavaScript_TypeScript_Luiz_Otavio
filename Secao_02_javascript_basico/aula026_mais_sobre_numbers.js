46c98d1fd71bd0b5372638418754022541e039c1
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-23 16:13:19 -0300

    JavaScript_TypeScript_Luiz_Otavio 23/07/2026 (tarde)

--- Secao_02_javascript_basico/aula026_mais_sobre_numbers.js
+++ Secao_02_javascript_basico/aula026_mais_sobre_numbers.js
@@ -1,17 +1,22 @@
 /*
     — Números em JavaScript:
 
-
+        O JS segue o padrão IEEE 754-2008.
+            
+            ↪ É o padrão técnico oficial para aritmética de ponto flutuante em computadores, definindo formatos para números binários e decimais, regras de arredondamento e tratamento de exceções.
 
 */
 
 
-let num1 = 1;
-let num2 = 2.5;
+let num1 = 10;
+let num2 = 0.7;
 let num3 = 1024;
-let num4 = 10.5789551255547;
+let num4 = 10.57;
 let num5 = '2026';
-let texto = 'Lorem ipsum';
+let num6 = 0.1;
+let valor1 = num2 * 'ola';
+let valor2 = num2 * num5;
+let valor3 = num2 + num6;
 
 
 // Convertendo número em string:
@@ -42,6 +47,16 @@ console.log();
 
 // Checando se o valor é um NaN (Not a Number):
 console.log('// Checando se o valor é um NaN (Not a Number):');
-console.log(Number.isNaN(num1));
-console.log(Number.isNaN(texto));
+console.log(valor1);
+console.log(valor2);
+console.log(Number.isNaN(valor1));
+console.log(Number.isNaN(valor2));
+console.log();
+
+
+// Inconsistência no padrão IEEE 754-2008:
+console.log('// Inconsistência no padrão IEEE 754-2008:');
+console.log(valor3);
+valor3 = Number(valor3.toFixed(1)); // Solução encontrada
+console.log(valor3);
 console.log();
