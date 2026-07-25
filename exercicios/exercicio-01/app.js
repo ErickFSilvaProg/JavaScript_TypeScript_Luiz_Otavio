d06578072e9ab86b7bac4ee522533c9dbd85169d
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-25 11:43:10 -0300

    JavaScript_TypeScript_Luiz_Otavio 25/07/2026 (manhã)

---
+++ exercicios/exercicio-01/app.js
@@ -0,0 +1,7 @@
+let nomeCompleto = prompt('Informe seu nome completo:');
+let nomesColetados = document.getElementById('nomes_coletados')
+
+console.log(nomes_coletados);
+console.log(nomeCompleto);
+
+nomes_coletados.innerHTML += `<p>${nomeCompleto}</p>`;
