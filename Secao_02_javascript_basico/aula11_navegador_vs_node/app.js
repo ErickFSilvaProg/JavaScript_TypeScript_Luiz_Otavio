6949ddcbe272f74af7a16aca50919a9e9ca2bbba
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-08-18 10:37:19 -0300

    JavaScript_TypeScript_Luiz_Otavio 18/08/2026 (manhã)

--- Secao_02_javascript_basico/aula11_navegador_vs_node/app.js
+++
@@ -1,7 +0,0 @@
-// Será executado no console do navegador.
-console.log('Olá mundo!');
-console.log('Este trecho será exibido no console do navegador.')
-
-// Será exibido em janela pop-pup no navegador.
-alert('Olá mundo!');
-alert('Meu nome é Erick 😁');
